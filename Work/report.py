f4fd059da7d3f3857120f48ebfd4a9fec71f315b
Lore <lorenabianchet@gmail.com>
2023-12-20 17:06:53 +0100

    End block 8.

--- Work/report.py
+++ Work/report.py
@@ -9,6 +9,15 @@ from stock import Stock
 from portfolio import Portfolio
 import tableformat
 
+# This file sets up basic configuration of the logging module.
+# Change settings here to adjust logging output as needed.
+import logging
+logging.basicConfig(
+    filename = 'app.log',            # Name of the log file (omit to use stderr)
+    filemode = 'w',                  # File mode (use 'a' to append)
+    level    = logging.WARNING,      # Logging level (DEBUG, INFO, WARNING, ERROR, or CRITICAL)
+)
+
 def read_portfolio(filename, **opts):
     '''
     Read a stock portfolio file into a list of dictionaries with keys
